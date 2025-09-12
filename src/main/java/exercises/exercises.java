9e1434e711d53cb93d8eb0db9272e4322ee29124
Ronald Espitia <ronald79@gmail.com>
2025-09-12 14:52:22 -0500

    Ejercicios

---
+++ src/main/java/exercises/exercises.java
@@ -0,0 +1,38 @@
+package exercises;
+
+public class exercises {
+    /*
+        1. Convertir un texto según la opción del usuario.
+           Opciones:
+            1 → Convertir a mayúsculas
+            2 → Convertir a minúsculas
+            3 → Capitalizar primera letra
+
+            test case 1:  input: texto = jupiter, opcion = 2
+            output: JUPITER
+
+            test case 2:  input: texto = SISTEMA SOLAR, opcion = 1
+            output: sistema solar
+
+            test case 3:  input: texto = neptuno, opcion = 3
+            output: Neptuno
+
+            test case 4:  input: texto = urano, opcion = 4
+            output: Opcion no valida
+
+
+        2. Verificar si una palabra o frase es palindromo
+           test case 1:  input: reconocer
+                         output: Es palindromo
+
+           test case 2:  input: saturno
+                         output: No es palindromo
+
+           test case 3:  input: anita lava la tina
+                         output: Es palindromo
+
+           test case 4:  input: hola mundo
+                         output: No es palindromo
+
+        */
+}
